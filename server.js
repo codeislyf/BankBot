bb7d9f8d75f4822e0e611accbc3c7a1896dd6bd2
codeislyf <utkarshas.2017@smu.edu.sg>
2017-12-11 17:34:09 +0800

    latest

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: '1ecfa9dc-4788-4540-9e06-b3acecc3c805' ,
+    appPassword: 'dbAT96)?lplziIHYNH622@]'
 });
 
 // Listen for messages
